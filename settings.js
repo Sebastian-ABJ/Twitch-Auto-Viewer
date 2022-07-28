fa805a3be3cc16a153aea3623cb0a17e6767ac2b
unknown <sebastian.abj@outlook.com>
2022-07-28 09:02:57 -0700

    Changed settings storage location to be in app data - we have persistence!

--- settings.js
+++ settings.js
@@ -1,7 +1,11 @@
+const electron = require('electron')
 const fs = require('fs')
+const path = require('path')
+const userDataPath = (electron.app || electron.remote.app).getPath('userData') + "/settings.json";
+console.log(userDataPath)
 
 function init() {
-  if (!fs.existsSync("./settings.json")) {
+  if (!fs.existsSync(userDataPath)) {
     console.log("No settings found, initializing to defaults...")
     settingsJSON = {
       "streamer": "None",
@@ -12,35 +16,35 @@ function init() {
       "display_ID": "",
       "client_ID": "9lyexvrvkjfh2mnygtma57mr7fp5a6"
     }
-    fs.writeFileSync('./settings.json', JSON.stringify(settingsJSON, null, 4))
+    fs.writeFileSync(userDataPath, JSON.stringify(settingsJSON, null, 4))
   }
 }
 
 function getSettings() {
-  return settingsJSON = require ('./settings.json')
+  return settingsJSON = require (userDataPath)
 }
 
 function update(streamer, speed, speedVal, displayID) {
-  var settingsJSON = require ('./settings.json')
+  var settingsJSON = require (userDataPath)
   settingsJSON.streamer = streamer
   settingsJSON.speed = speed
   settingsJSON.speedVal = speedVal
   settingsJSON.display_ID = displayID
-  fs.writeFileSync('./settings.json', JSON.stringify(settingsJSON, null, 4))
+  fs.writeFileSync(userDataPath, JSON.stringify(settingsJSON, null, 4))
   console.log("Settings updated.")
 }
 
 function updateToken(token) {
-  var settingsJSON = require ('./settings.json')
+  var settingsJSON = require (userDataPath)
   settingsJSON.token = token
-  fs.writeFileSync('./settings.json', JSON.stringify(settingsJSON, null, 4))
+  fs.writeFileSync(userDataPath, JSON.stringify(settingsJSON, null, 4))
   console.log("Updated token.")
 }
 
 function updateValidationTime(validationTime) {
-  var settingsJSON = require ('./settings.json')
+  var settingsJSON = require (userDataPath)
   settingsJSON.validation_time = validationTime
-  fs.writeFileSync('./settings.json', JSON.stringify(settingsJSON, null, 4))
+  fs.writeFileSync(userDataPath, JSON.stringify(settingsJSON, null, 4))
   console.log("Updated token validation time.")
 }
 
